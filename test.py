9a3228110b5c6fab5dfba4dadd6e149e44ef20c0
Shee69 <lehoangnamtep@gmail.com>
2023-06-21 10:52:25 +0700

    Setup

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import cv2 as cv
 import numpy as np
-import os
 
-img = cv.imread('cat1.jpg')
+img = cv.imread('human1.jpg')
 assert img is not None, "file could not be read, check with os.path.exists()"
 
+# Cartoon filter (?)
+kernel = np.ones((5,5),np.uint8)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 gray = cv.GaussianBlur(gray,(5,5),-1)
 edges = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,9,10)
-color = cv.bilateralFilter(img, 20, 245, 245)
+edges = cv.morphologyEx(edges, cv.MORPH_OPEN, kernel)
+color = cv.bilateralFilter(img, 10, 10, 245)
 cartoon = cv.bitwise_and(color, color, mask=edges)
+
 cv.imshow("cartoon",cartoon)
 cv.waitKey(0)
 cv.destroyAllWindows()
